380f1c79fc3d27aea384864da4f65d5679f05bdb
Christian Faatoafe <chris_sila39@hotmail.com>
2020-10-12 16:09:29 -0800

    added card body

---
+++ Assets/script.js
@@ -0,0 +1,67 @@
+var city = $("#city").val();
+
+var apiKey = "&appid=d7ea103782caf604118a57f32d78ef2f";
+
+$("#searchBtn").on("click", function (event) {
+    event.preventDefault();
+    city = $("#city").val();
+    $("#city").val("");
+
+    var queryUrl = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + apiKey;
+
+    $.ajax({
+        url: queryUrl,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        console.log(response.city.name);
+        console.log(response.list[0].weather[0].icon);
+        var tempF = (response.list[0].main.temp - 273.15) * 1.80 + 32;
+        console.log(Math.floor(tempF));
+        console.log(response.list[0].main.humidity);
+        console.log(response.list[0].wind.speed);
+
+        currentWeather(response);
+        currentForcast(response);
+        makeList();
+    });
+});
+
+function makeList() {
+    var listItem = $("<li>").addClass("list-group-item").text(city);
+    $(".list").append(listItem);
+}
+
+function currentWeather(response) {
+    let tempF = (response.list[0].main.temp - 273.15) * 1.80 + 32;
+    tempF = Math.floor(tempF);
+    $("#city").empty();
+
+    var card = $("<div>").addClass("card");
+    var cardBody = $("<div>").addClass("card-body");
+    var currentCity = $("<h4>").addClass("card-title").text(response.city.name);
+    var cityDate = $("<h4>").addClass("card-title").text(response.list.dt);
+    var temperature = $("<p>").addClass("card-text current-temp").text("Temperature: " + tempF + " °F");
+    var humidity = $("<p>").addClass("card-text current-humidity").text("Humidity: " + response.list[0].main.humidity + "%");
+    var wind = $("<p>").addClass("card-text current-wind").text("Wind Speed: " + response.list[0].wind.speed + "MPH");
+    var image = $("<img>").attr("src", "https://openweathermap.org/img/wn/" + response.list[0].weather[0].icon + "@2x.png");
+
+    currentCity.append(cityDate, image);
+    cardBody.append(currentCity, temperature, humidity, wind);
+    card.append(cardBody);
+    $("#currentCity").append(card);
+};
+
+function currentForcast() {
+    $.ajax({
+        url: "https://api.openweathermap.org/data/2.5/forecast?q=" + city + apiKey,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        console.log(response.list.dt);
+        $("#forecast").empty();
+
+        var results = response.list;
+        console.log(results);
+    })
+}
